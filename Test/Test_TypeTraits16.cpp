bb47f34a1829f149a06c86cfdc90c45ee49fb1f1
Erik Olofsson <erik@olofsson.info>
2026-04-05 17:21:48 +0200

    Update licensing to Apache 2.0 with LLVM Exceptions

--- Test/Test_TypeTraits16.cpp
+++ Test/Test_TypeTraits16.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2015 Hansoft AB
-// Distributed under the MIT license, see license text in LICENSE.Malterlib
+// Copyright © Unbroken AB
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "Test_TypeTraits_TestTypes.h"
